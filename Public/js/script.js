d44d63d50e5cb4c327f17e837aa08f1c21b08356
Salvador Tapia <salvadortapia324@gmail.com>
2024-10-11 12:09:48 -0400

    dgfj

--- Public/js/script.js
+++ Public/js/script.js
@@ -4,5 +4,5 @@ console.log("JavaScript is running!");
 // Example: Change the text of an h1 element
 document.addEventListener('DOMContentLoaded', function() {
     const heading = document.querySelector('h1');
-    heading.textContent = "Welcome to DYC Project!";
+    heading.textContent = "A place that holds all information for the Diocesan Youth Council (DYC).";
 });

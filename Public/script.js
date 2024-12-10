cc8a2103e49188e8da1bd10771f699951a8a0e9c
Salvador Tapia <salvadortapia324@gmail.com>
2024-12-10 11:03:57 -0500

    add js

---
+++ Public/script.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none"; // Hide the dropdown
+    } else {
+        dropdownContent.style.display = "block"; // Show the dropdown
+    }
+}

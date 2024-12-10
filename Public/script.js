e1946243ee05995dc0e5c9ea528c0f435ed23369
Salvador Tapia <salvadortapia324@gmail.com>
2024-12-10 11:36:54 -0500

    add to fix the drop down

--- Public/script.js
+++ Public/script.js
@@ -1,9 +1,21 @@
-// script.js
 function toggleDropdown() {
-    var dropdownContent = document.getElementById("dropdown-content");
+    var dropdownContent = document.getElementById("dropdownContent");
     if (dropdownContent.style.display === "block") {
         dropdownContent.style.display = "none"; // Hide the dropdown
     } else {
         dropdownContent.style.display = "block"; // Show the dropdown
+    }
+}
+
+// Optional: Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
     }
 }
